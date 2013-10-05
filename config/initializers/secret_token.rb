cb5bfe955bc843f6db31f5058dd7658481d0d236
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2013-10-05 13:45:50 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursetracker::Application.config.secret_token = 'd40fc2e0b94b99f15a8d5413f79a92331d43a0d858029d715252f46c367f8939c6144aca71fd889fa9f7bec2f1423e86f10bde40716fc0ba26c805ddcfd56717'
